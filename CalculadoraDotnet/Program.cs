3820cf74cd5f8b41967a4a4ab4aae0aa3368331d
Reinan Silveira <reinanlanz@gmail.com>
2024-06-09 21:27:59 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraDotnet/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class CalculadoraDotnet
+{
+    private static int result;
+
+    public static void Main()
+    { 
+        Console.WriteLine("Digite um número");
+        var leftValue = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Digite o Operador");
+
+        var operatorValue = Console.ReadLine();
+
+        Console.WriteLine("Digite outro número");
+
+        var rightValue = Convert.ToInt16(Console.ReadLine());
+        
+
+
+        switch (operatorValue)
+        {
+            case "+":
+                result = leftValue + rightValue;
+                break;
+            case "-" :
+                result = leftValue - rightValue;
+                break;
+            case "*":
+                result = leftValue * rightValue;
+                break;
+            case "/":
+                result = leftValue / rightValue;
+                break;
+        }
+
+        Console.WriteLine($"O resultado é {result}");   
+
+
+
+        
+
+ 
+
+}
+}
+
+  
+        
+
+
+   
+      
+
+    
+    
+
+
+
